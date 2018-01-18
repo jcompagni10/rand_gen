a2e9a46f0d52a3ae7a964a0d0d1f370990b5f3e2
Julian Compagni Portis <jcompagni@gmail.com>
2018-01-17 18:26:28 -0800

    change to class and add frequency tracking

--- lib/rand_gen.rb
+++ lib/rand_gen.rb
@@ -1,19 +1,53 @@
-def rand_gen
-  rand100 = rand(1..100)
-  case 
-  when (0..50).cover?(rand100)
-    puts 1
-  when (51..75).cover?(rand100)
-    puts 2
-  when (76..90).cover?(rand100)
-    puts 3
-  when (91..95).cover?(rand100)
-    puts 4
-  when (96..100).cover?(rand100)
-    puts 4
+require "byebug"
+
+class RandomGenerator
+  attr_reader :store
+  def initialize
+    @store = []
+  end
+
+  def add_to_store(val)
+    @store << val
+    @store.shift if @store.length > 100
+  end
+
+  def handle_output(val)
+    p val
+    add_to_store(val)
+  end
+
+  def get_freq
+    freq = Hash.new(0)
+    @store.each do |val|
+      freq[val] += 1
+    end
+    freq.each do |key, val|
+      freq[key] = val / @store.length.to_f
+    end
+    freq
+  end
+
+  def rand_gen
+    rand100 = rand(1..100)
+    if (0..50).cover?(rand100)
+      output_val =  1
+    elsif (51..75).cover?(rand100)
+      output_val =  2
+    elsif (76..90).cover?(rand100)
+      output_val = 3
+    elsif (91..95).cover?(rand100)
+      output_val =  4
+    elsif (96..100).cover?(rand100)
+      output_val =  5
+    end
+    handle_output(output_val)
   end
 end
 
-  100.times do
-    rand_gen
-  end
+
+#test 
+rg = RandomGenerator.new
+100.times do
+  rg.rand_gen
+end
+puts rg.get_freq
